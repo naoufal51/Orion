03f77d90fdd4ac0065ad75c8601ff6bc00f8c9af
Naoufal <naoufal51@gmail.com>
2018-04-03 16:04:50 +0200

    modif

--- code/sm_matrices.py
+++ code/sm_matrices.py
@@ -7,18 +7,35 @@ Naoufal Mahfoudi (c) 2016 mohamed-naoufal.mahfoudi@inria.fr
 """
 import numpy as np
 
+# sm_1 = 1
+#
+# sm_2_20 = np.matrix('1 1; 1 -1') / np.sqrt(2)
+#
+# sm_2_40 = np.matrix('1 1; 1j 1') / np.sqrt(2)
+#
+# sm_3_20 = np.matrix('-2*np.pi/16 -2*np.pi/(80/33) 2*np.pi/(80/3);'
+#                     '2*np.pi/(80/23) 2*np.pi/(48/13) 2*np.pi/(240/13);'
+#                     '-2*np.pi/(80/13) 2*np.pi/(240/37) 2*np.pi/(48/13)')
+# sm_3_20 = np.exp(1j * sm_3_20) / np.sqrt(3)
+#
+# sm_3_40 = np.matrix('-2*np.pi/16 -2*np.pi/(80/13) 2*np.pi/(80/23);'
+#                     '2*np.pi/(80/37) 2*np.pi/(48/11) 2*np.pi/(240/107);'
+#                     '-2*np.pi/(80/7) 2*np.pi/(240/83) 2*np.pi/(48/11)')
+# sm_3_20 = np.exp(1j * sm_3_40) / np.sqrt(3)
+
 sm_1 = 1
 
-sm_2_20 = np.matrix('1 1; 1 -1') / np.sqrt(2)
+sm_2_20 = np.matrix([[1, 1], [1, -1]]) / np.sqrt(2)
 
-sm_2_40 = np.matrix('1 1; 1j 1') / np.sqrt(2)
+sm_2_40 = np.matrix([[1, 1], [1j, 1]]) / np.sqrt(2)
 
-sm_3_20 = np.matrix('-2*np.pi/16 -2*np.pi/(80/33) 2*np.pi/(80/3);'
-                    '2*np.pi/(80/23) 2*np.pi/(48/13) 2*np.pi/(240/13);'
-                    '-2*np.pi/(80/13) 2*np.pi/(240/37) 2*np.pi/(48/13)')
+sm_3_20 = np.matrix([[-2 * np.pi / 16, -2 * np.pi / (80 / 33), 2 * np.pi / (80 / 3)],
+                     [2 * np.pi / (80 / 23), 2 * np.pi / (48 / 13), 2 * np.pi / (240 / 13)],
+                     [-2 * np.pi / (80 / 13), 2 * np.pi / (240 / 37), 2 * np.pi / (48 / 13)]])
+
 sm_3_20 = np.exp(1j * sm_3_20) / np.sqrt(3)
 
-sm_3_40 = np.matrix('-2*np.pi/16 -2*np.pi/(80/13) 2*np.pi/(80/23);'
-                    '2*np.pi/(80/37) 2*np.pi/(48/11) 2*np.pi/(240/107);'
-                    '-2*np.pi/(80/7) 2*np.pi/(240/83) 2*np.pi/(48/11)')
-sm_3_20 = np.exp(1j * sm_3_40) / np.sqrt(3)
+sm_3_40 = np.matrix([[-2 * np.pi / 16, -2 * np.pi / (80 / 13), 2 * np.pi / (80 / 23)],
+                     [2 * np.pi / (80 / 37), 2 * np.pi / (48 / 11), 2 * np.pi / (240 / 107)],
+                     [-2 * np.pi / (80 / 7), 2 * np.pi / (240 / 83), 2 * np.pi / (48 / 11)]])
+sm_3_40 = np.exp(1j * sm_3_40) / np.sqrt(3)
